4ed5816df432dd653fa3de6c06e86bf8008b80e6
Derqice <139995816+Derqice@users.noreply.github.com>
2025-11-09 20:36:28 +0000

    will redo when unexpired api key is aquired

--- index.js
+++ index.js
@@ -5,34 +5,46 @@
 // 1. Look through the Spotify API Documentation and find the end point for creating a playlist.
 // Update the variable below called `createPlaylistURL` and add the URL, the user_id will be 31s5fcbgzo3pkww6ancnzov63cjm
 
-let createPlaylistURL = "";
+let createPlaylistURL = "https://api.spotify.com/v1/user/31s5fcbgzo3pkww6ancnzov63cjm/playlists";
 
 // 2. The documentation describes how to create the request body. Using that information update the object below called `newPlaylistBody`
 // Add the name of the playlist, make sure it's public, and add a description with your name and section (+ whatever else you want)
 
 const newPlaylistBody = {
+  name: "poopypants",
+  public: true,
+  description: "Derquan, SE12 Period 5, A whole bunch of songs about pure donkey sh*t"
 
 };
 
 // 3. Add `playlistData` as a parameter to the asynchronous function `createPlaylist`.
 
-async function createPlaylist() {
+async function createPlaylist(playlistData) {
 
   // 4. Build out the options object.
   //    - Set the HTTP Method
   //    - Add the headers Authorization and Content-Type, the API Key will be on G-Chat. Format it like so: "Bearer <Your API Key>"
   //    - Add the `newPlaylistBody` object into the body, make sure you stringify it
 
-  const options = {};
+  const options = {
+    method: "POST",
+    headers:{
+      Authorization: "Bearer ",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newPlaylistBody)
+  };
 
   // 5. Use fetch() to send a POST request using the `createPlaylistURL` and options object. Console log the response.
-  
+  let response= await fetch (createPlaylistURL, options)
+  let data= await response.json()
+  console.log(data)
 }
 
 // 6. Call your function `createPlaylist` with `newPlaylistBody` passed as the parameter. Take note of the ID it will be needed in the next request.
 // Go to the Spotify Profile to see the playlist you created. (Or follow the URL in the response)
 // https://open.spotify.com/user/31s5fcbgzo3pkww6ancnzov63cjm
-
+createPlaylist(newPlaylistBody)
 
 
 // Add Songs to Playlist:
@@ -40,25 +52,33 @@ async function createPlaylist() {
 // 1. Look through the Spotify API Documentation and find the end point for adding songs to a playlist.
 // Update the variable below called `addingSongsURL` and add the URL, use the playlist_id that you recieved from the function above
 
-let addingSongsURL = "";
+let addingSongsURL = "https://api.spotify.com/v1/playlists/(didn't get one because api key expired)/tracks";
 
 // 2. Find 4 songs on spotify and add them to the songs array below. (If spotify is blocked use the example below, or ask Mr.Castro)
 //    - The songs should be in the format: "spotify:track:<track_id>"
 //    - Find the track id from the songs URL after track/ but before the queries
 //    - Ex: https://open.spotify.com/track/6a6JDEQbXdxkWFRIA0pRqL?si=e9e0c29125a746f6 => spotify:track:6a6JDEQbXdxkWFRIA0pRqL
 
-const songs = [];
+const songs = [
+  "spotify:track:6qBFSepqLCuh5tehehc1bd",
+  "spotify:track:3LBM25lgxl9y549SiYxtSi",
+  "spotify:track:6qvtoU0gbkaOcTx6mnVDgV",
+  "spotify:track:4rHlfYRQw6ittRQXAlurk0"
+];
 
 // 3. The documentation describes how to create the request body. Using that information update the object below called `newSongsBody`
 // Add the array `songs` into this object
 
 const newSongsBody = {
+  playlist_id:"(didn't get one because api key expired)",
+  position:0,
+  uris:songs,
 
 };
 
 // 4. Add `songData` as a parameter to the asynchronous function `addPlaylistSongs`.
 
-async function addPlaylistSongs() {
+async function addPlaylistSongs(songData) {
 
   // 5. Inside the function build out the options object.
   //    - Set the HTTP Method
@@ -66,11 +86,20 @@ async function addPlaylistSongs() {
   //    - Add the `newSongsBody` object into the body, make sure you stringify it
 
   const options = {
-    
+    method: "POST",
+    headers: {
+      Authorization: "Bearer ",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newSongsBody)
   };
 
   // 6. Use fetch() to send a POST request using the `addingSongsURL` and options object. Console log the response.
+  let response = await fetch(addingSongsURL,options)
+  let data= await response.json()
+  console.log(data)
 }
 
 // 7. Call your function `addPlaylistSongs` with `newSongsBody` passed as the parameter.
 // Refresh the page to your playlist, you should see your 3 new songs.
+addPlaylistSongs(newSongsBody)
